4784d53051d2de7747f32177e7b90ce8d4eb6e90
mmiah00 <mmiah00@stuy.edu>
2020-03-01 17:04:15 +0000

    wrote swap

--- sudokuswap.py
+++ sudokuswap.py
@@ -6,43 +6,13 @@ class box: #one square on a board
         self.id = id #position in board
 
 class board:
-    cliques=[[0,1,2,3,4,5,6,7,8],
-    [9,10,11,12,13,14,15,16,17],
-    [18,19,20,21,22,23,24,25,26],
-    [27,28,29,30,31,32,33,34,35],
-    [36,37,38,39,40,41,42,43,44],
-    [45,46,47,48,49,50,51,52,53],
-    [54,55,56,57,58,59,60,61,62],
-    [63,64,65,66,67,68,69,70,71],
-    [72,73,74,75,76,77,78,79,80],
-    [0,9,18,27,36,45,54,63,72],
-    [1,10,19,28,37,46,55,64,73],
-    [2,11,20,29,38,47,56,65,74],
-    [3,12,21,30,39,48,57,66,75],
-    [4,13,22,31,40,49,58,67,76],
-    [5,14,23,32,41,50,59,68,77],
-    [6,15,24,33,42,51,60,69,78],
-    [7,16,25,34,43,52,61,70,79],
-    [8,17,26,35,44,53,62,71,80],
-    [0,1,2,9,10,11,18,19,20],
-    [3,4,5,12,13,14,21,22,23],
-    [6,7,8,15,16,17,24,25,26],
-    [27,28,29,36,37,38,45,46,47],
-    [30,31,32,39,40,41,48,49,50],
-    [33,34,35,42,43,44,51,52,53],
-    [54,55,56,63,64,65,72,73,74],
-    [57,58,59,66,67,68,75,76,77],
-    [60,61,62,69,70,71,78,79,80]]
+    data = dict ()
 
     def __init__ (self, file):
-        bored = parser (file)
-        self.boxes = setup (bored)
-        # b = file.split ("\n")
-        # bored = []
-        # for i in range (len (b)):
-        #     bored.append (b[i].split (","))
+        bored = self.parser (file)
+        self.boxes = self.setup (bored)
 
-    def parser (file): #returns an array parsed with all the values in the board
+    def parser (self,file): #returns an array parsed with all the values in the board
         fi = open (file, "r")
         ans = []
         for line in fi:
@@ -54,24 +24,66 @@ class board:
             ans.remove ([''])
         return ans
 
-    def setup (board): #takes parsed board and makes and array of boxes holding the id and the data
+    def setup (self,bored): #takes parsed board and makes and array of boxes holding the id and the data
         id = 0
         ans = []
-        for r in board:
+        for r in bored:
             for c in r:
                 num = int (c)
                 new_box = box (num, id)
                 ans.append (new_box)
+                self.data[id] = num
                 id += 1
         return ans
 
-    def check (clique):
+    def check (clique): #takes a row, column, or group and checks if there are doubles
         clique.sort ()
         nums = [1,2,3,4,5,6,7,8,9]
         for i in range (len (nums)):
             if nums[i] != clique[i]:
                 return False
         return True
-        
-a = parser ("tester.txt")
-print (a)
+
+    def swap (self, id1, id2): #takes the id of the spots you want to swap and swaps the values
+        one = self.data[id1]
+        another = self.data[id2]
+        self.data[id1] = another
+        self.data[id2] = one
+        if one < another:
+            return ("" + str (one) + "," + str(another) + "\n")
+        else:
+            return ("" + str(another) + "," + str(one) + "\n")
+
+
+cliques=[[0,1,2,3,4,5,6,7,8],
+[9,10,11,12,13,14,15,16,17],
+[18,19,20,21,22,23,24,25,26],
+[27,28,29,30,31,32,33,34,35],
+[36,37,38,39,40,41,42,43,44],
+[45,46,47,48,49,50,51,52,53],
+[54,55,56,57,58,59,60,61,62],
+[63,64,65,66,67,68,69,70,71],
+[72,73,74,75,76,77,78,79,80],
+[0,9,18,27,36,45,54,63,72],
+[1,10,19,28,37,46,55,64,73],
+[2,11,20,29,38,47,56,65,74],
+[3,12,21,30,39,48,57,66,75],
+[4,13,22,31,40,49,58,67,76],
+[5,14,23,32,41,50,59,68,77],
+[6,15,24,33,42,51,60,69,78],
+[7,16,25,34,43,52,61,70,79],
+[8,17,26,35,44,53,62,71,80],
+[0,1,2,9,10,11,18,19,20],
+[3,4,5,12,13,14,21,22,23],
+[6,7,8,15,16,17,24,25,26],
+[27,28,29,36,37,38,45,46,47],
+[30,31,32,39,40,41,48,49,50],
+[33,34,35,42,43,44,51,52,53],
+[54,55,56,63,64,65,72,73,74],
+[57,58,59,66,67,68,75,76,77],
+[60,61,62,69,70,71,78,79,80]]
+
+a = board ("tester.txt")
+print (a.data)
+print (a.swap (0,1))
+print (a.data)
