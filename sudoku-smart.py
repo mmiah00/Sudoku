ef6ea869f1b751a00340f7129348333542a28515
mmiah00 <mmiah00@stuy.edu>
2020-03-29 21:10:12 +0100

    uploaded to website, not the best but im lazy

--- sudoku-smart.py
+++ sudoku-smart.py
@@ -278,7 +278,6 @@ for i in range (len (tests)):
     title = tests[i].name.split (",")[0]
     if title == board_name:
         o = open (output, "w")
-        o.write (tests[i].name + "\n") #he didn't want this but i just kept it in there
         tests[i].solve ()
         print ("Trials: ", tests[i].trials, " Backtracks: ", tests[i].backtracks)
         print (tests[i].board_string ())
